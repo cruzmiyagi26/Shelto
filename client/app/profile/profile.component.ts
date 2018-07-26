f04bf2c0ba960a71176b3622969f3a2ad1715428
MacBook Air <macbookair@lad-cust-129-122-163-202.zapfibra.co.ao>
2018-07-25 20:30:05 -0400

    first commit

---
+++ client/app/profile/profile.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../_models/index';
+import { UserService } from '../_services/index';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'profile.component.html',
+    styleUrls: ['profile.component.css']
+    })
+export class ProfileComponent implements OnInit {
+    currentUser: User;
+    users: User[] = [];
+
+    constructor(private userService: UserService) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+
+    ngOnInit() {
+    }
+}
